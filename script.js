cdef08fa19e6fc31c85c2ee981ca786eb9cde0ed
J Chevalier <your_email@example.com>
2026-05-06 20:54:38 -0400

    adding htmls

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log("Hello Javascript ");
+console.log("Jenny Chevalier");
+
+//alert("Welcome to javascript");
+
+
+//variables
+
+let name = "Jenny";
+let age = 24;
+
+console.log(name)
+console.log(age)
+
+let name1 = "Ice";
+let name2 = "Cream";
+let age1 = 23;
+let age2 = 7;
+
+console.log(name1);
+console.log(name2);
+console.log(age1);
+console.log(age2);
+
+//function = reusable block of code.
+
+//function greet() {
+
+//alert("hello class");
+
+//}
+//greet();
+
+
+function show() {
+    alert("button is clicked")
+}
+
+function show() {
+    alert("This has been submitted")
+}
---
+++ script2.js
@@ -0,0 +1,38 @@
+console.log("Practice page")
+console.log("Jenny Chevalier")
+
+alert("Welcome to the Practice Page")
+
+// variables
+
+let name = "Practice";
+let age = 23;
+
+console.log(name)
+console.log(age)
+
+let name1 = "New";
+let name2 = "Day";
+let age1 = 6;
+let age2 = 22;
+
+console.log(name1);
+console.log(name2);
+console.log(age1);
+console.log(age2);
+
+
+function greet() {
+
+    alert("hello class");
+}
+
+function show() {
+    alert("button is clicked")
+}
+
+function show() {
+    alert("This has been submitted")
+}
+
+document.getElementById("class").textContent = "This text was changed with JavaScript!";
