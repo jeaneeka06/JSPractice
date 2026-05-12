7576f6a1a717b0c2e1551e01bffc391d92368ed0
J Chevalier <your_email@example.com>
2026-05-11 20:57:36 -0400

    class 5/11

--- script.js
+++ script.js
@@ -1,41 +1,119 @@
-console.log("Hello Javascript ");
-console.log("Jenny Chevalier");
+// // // // //console.log("Hello Javascript ");
+// // // // console.log("Jenny Chevalier");
 
-//alert("Welcome to javascript");
-
-
-//variables
-
-let name = "Jenny";
-let age = 24;
-
-console.log(name)
-console.log(age)
-
-let name1 = "Ice";
-let name2 = "Cream";
-let age1 = 23;
-let age2 = 7;
-
-console.log(name1);
-console.log(name2);
-console.log(age1);
-console.log(age2);
-
-//function = reusable block of code.
-
-//function greet() {
-
-//alert("hello class");
-
-//}
-//greet();
+// // // // //alert("Welcome to javascript");
 
 
-function show() {
-    alert("button is clicked")
+// // // // //variables
+
+// // // // let name = "Jenny";
+// // // // let age = 24;
+
+// // // // console.log(name)
+// // // // console.log(age)
+
+// // // // let name1 = "Ice";
+// // // // let name2 = "Cream";
+// // // // let age1 = 23;
+// // // // let age2 = 7;
+
+// // // // console.log(name1);
+// // // // console.log(name2);
+// // // // console.log(age1);
+// // // // console.log(age2);
+
+// // // // //function = reusable block of code.
+
+// // // // //function greet() {
+
+// // // // //alert("hello class");
+
+// // // // //}
+// // // // //greet();
+
+
+// // // // function show() {
+// // // //     alert("button is clicked")
+// // // // }
+
+// // // // function show() {
+// // // //     alert("This has been submitted")
+// // // // }
+
+
+// // // let age = 18;
+
+// // // if (age >= 18) {
+// // //     console.log("Adult");
+// // // }
+// // // else {
+// // //     console.log("Minor");
+// // // }
+
+// // // let password = "1234";
+
+// // // if (password == "1234") {
+// // //     alert("Login Successful");
+// // // }
+// // // else {
+// // //     alert("Wrong Password");
+// // // }
+
+// // function checkage() {
+
+// //     let age = document.getElementById("age").value;
+
+// //     if (age >= 18) {
+// //         alert("you can vote");
+// //     }
+// //     else {
+// //         alert("you are underage");
+
+// //     }
+
+// // }
+
+// // function checkName() {
+// //     const name = document.getElementById("username").value;
+
+// //     if (name === "") {
+// //         alert("Please enter a name.");
+// //     } else {
+// //         alert("Hello " + name + "!");
+// //     }
+// // }
+
+// let marks = 85;
+
+// if (marks >= 90) {
+//     console.log("A");
+// }
+
+// else if (marks >= 80) {
+//     console.log("B");
+// }
+
+// else if (marks >= 65) {
+//     console.log("C");
+// }
+
+// else {
+//     console.log("Fail");
+// }
+
+// loops
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
 }
 
-function show() {
-    alert("This has been submitted")
+for (let a = 100; a >= 1; a--) {
+    console.log(a);
 }
+
+
+
+
+
+
+
